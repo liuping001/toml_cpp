97f2d0d00a782e5cd2bbb11a7d524d072fc848c6
liuping001 <liuping08093271>
2019-12-14 15:00:44 +0800

    modify

--- toml_make_cpp.cpp
+++ toml_make_cpp.cpp
@@ -6,9 +6,13 @@
 #include <iostream>
 #include <memory>
 
+namespace toml_cpp {
+
+static std::string Tab;
+static std::string toml_base_dir;
+
 struct CppOut {
   std::ostringstream make_struct;
-  std::ostringstream toml_cpp;
 };
 
 inline std::string BigWord(const std::string &org_word) {
@@ -19,48 +23,49 @@ inline std::string BigWord(const std::string &org_word) {
   return word;
 }
 
-void PrintArray(std::shared_ptr<cpptoml::base> ptr, CppOut &ss) {
+void StructDefineArray(std::shared_ptr<cpptoml::base> ptr, CppOut &ss) {
   if (ptr->is_array()) {
     ss.make_struct << "std::vector<";
-    PrintArray(*ptr->as_array()->begin(), ss);
+    StructDefineArray(*ptr->as_array()->begin(), ss);
     ss.make_struct << ">";
   } else {
     ss.make_struct << "TomlBase";
   }
 }
 
-
-void Print(std::shared_ptr<cpptoml::base> ptr, const std::string &key, CppOut &ss, std::string depth) {
-  auto next_depth = depth + "    ";
+void MakeStructDefine(std::shared_ptr<cpptoml::base> ptr,
+                      const std::string &key,
+                      CppOut &ss,
+                      const std::string &depth) {
+  auto next_depth = depth + Tab;
   if (ptr->is_array()) {
     ss.make_struct << depth;
-    PrintArray(ptr, ss);
+    StructDefineArray(ptr, ss);
     ss.make_struct << " " << key << ";\n";
   } else if (ptr->is_table()) {
-    ss.make_struct << depth<< BigWord(key) <<" "<< key <<"; \n";
+    ss.make_struct << depth << BigWord(key) << " " << key << "; \n";
   } else if (ptr->is_table_array()) {
-    ss.make_struct << depth << "std::vector<" << BigWord(key) <<"> " << key << ";\n";
+    ss.make_struct << depth << "std::vector<" << BigWord(key) << "> " << key << ";\n";
   } else {
-    ss.make_struct <<depth << "TomlBase "<< key <<";\n";
+    ss.make_struct << depth << "TomlBase " << key << ";\n";
   }
 }
 
-
-void InitPrintArray(std::shared_ptr<cpptoml::base> ptr, std::ostringstream &init_func,std::string depth,
-    const std::string &key, int32_t d) {
-  std::string item1 = "arr_" + key + "_item" + std::to_string(d -1);
+void MakeInitArray(std::shared_ptr<cpptoml::base> ptr, std::ostringstream &init_func, const std::string &depth,
+                   const std::string &key, int32_t d) {
+  std::string item1 = "arr_" + key + "_item" + std::to_string(d - 1);
   std::string item2 = "arr_" + key + "_item" + std::to_string(d);
 
-  auto n_depth = depth + "    ";
-  auto nn_depth = n_depth + "    ";
+  auto n_depth = depth + Tab;
+  auto nn_depth = n_depth + Tab;
   auto first_item = ptr->is_array() ? *ptr->as_array()->begin() : nullptr;
   if (first_item && first_item->is_array()) {
-    init_func << n_depth<< "for (auto item : *arr_"<< key<< "->as_array()) {\n";
+    init_func << n_depth << "for (auto item : *arr_" << key << "->as_array()) {\n";
     init_func << nn_depth << "auto arr_data = item;\n";
-    init_func << nn_depth <<  "decltype("<<item1 <<")::value_type "<< item2<< ";\n";
-    InitPrintArray(first_item, init_func, n_depth, key, d + 1);
-    init_func << nn_depth << item1 <<".push_back(" << item2 <<");\n";
-    init_func <<n_depth<< "}\n";
+    init_func << nn_depth << "decltype(" << item1 << ")::value_type " << item2 << ";\n";
+    MakeInitArray(first_item, init_func, n_depth, key, d + 1);
+    init_func << nn_depth << item1 << ".push_back(" << item2 << ");\n";
+    init_func << n_depth << "}\n";
   } else {
     if (ptr->is_table_array()) {
       init_func << n_depth << "for (auto item : *arr_" << key << "->as_table_array()) {\n";
@@ -75,82 +80,105 @@ void InitPrintArray(std::shared_ptr<cpptoml::base> ptr, std::ostringstream &init
   }
 }
 
-void InitPrint(std::shared_ptr<cpptoml::base> ptr, const std::string &key, std::ostringstream &init_func, std::string depth) {
-  auto n_depth = depth + "    ";
+void MakeInitFunc(std::shared_ptr<cpptoml::base> ptr,
+                  const std::string &key,
+                  std::ostringstream &init_func,
+                  const std::string &depth) {
+  auto n_depth = depth + Tab;
   if (ptr->is_array() || ptr->is_table_array()) {
     auto d = 1;
-    init_func << n_depth << "auto arr_" << key<<" = ptr->as_table()->get(\"" << key << "\");\n";
-    init_func << n_depth << "decltype("<<key<<") arr_"<<key<<"_item"<< d <<";\n";
-    InitPrintArray(ptr, init_func, depth, key, d + 1);
-    init_func << n_depth << key <<" = " <<  "std::move(arr_"<<key<<"_item"<< d <<");\n";
-  }else if (ptr->is_table()) {
-    init_func << n_depth << key <<".FromToml(ptr->as_table()->get(\""<< key<<"\"));\n";
+    init_func << n_depth << "auto arr_" << key << " = ptr->as_table()->get(\"" << key << "\");\n";
+    init_func << n_depth << "decltype(" << key << ") arr_" << key << "_item" << d << ";\n";
+    MakeInitArray(ptr, init_func, depth, key, d + 1);
+    init_func << n_depth << key << " = " << "std::move(arr_" << key << "_item" << d << ");\n";
+  } else if (ptr->is_table()) {
+    init_func << n_depth << key << ".FromToml(ptr->as_table()->get(\"" << key << "\"));\n";
   } else {
-    init_func << n_depth << key << " = ptr->as_table()->get(\""<<key<<"\");\n";
+    init_func << n_depth << key << " = ptr->as_table()->get(\"" << key << "\");\n";
   }
 }
 
-void PrintStruct(std::shared_ptr<cpptoml::base> ptr, const std::string &key, CppOut &ss, std::string depth) {
-  auto n_depth = depth + "    ";
-  auto nn_depth = n_depth + "    ";
+// 由于依赖关系，所以先遍历到最低层数据结构 然后才分析结构
+void PrintStruct(std::shared_ptr<cpptoml::base> ptr, const std::string &key, CppOut &ss, const std::string &depth) {
+  auto n_depth = depth + Tab;
+  auto nn_depth = n_depth + Tab;
   std::vector<std::pair<std::string, std::shared_ptr<cpptoml::base>>> depend;
-  if (ptr->is_table()) {
-    for (auto &item : *ptr->as_table()) {
-      if (item.second->is_table() || item.second->is_table_array()) {
-        depend.emplace_back(item.first, item.second);
-      }
-    }
-  } else if (ptr->is_table_array()) {
-    for (auto &item : **ptr->as_table_array()->begin()) {
+  if (ptr->is_table() || ptr->is_table_array()) {
+    auto &type_table = ptr->is_table() ? *ptr->as_table() : **ptr->as_table_array()->begin();
+    for (auto &item : type_table) {
       if (item.second->is_table() || item.second->is_table_array()) {
         depend.emplace_back(item.first, item.second);
       }
     }
   }
 
-  for(auto item : depend) {
+  for (auto item : depend) {
     PrintStruct(item.second, item.first, ss, depth);
   }
 
   std::ostringstream init_func;
-  if (ptr->is_table()) {
+  if (ptr->is_table() || ptr->is_table_array()) {
+    auto &type_table = ptr->is_table() ? *ptr->as_table() : **ptr->as_table_array()->begin();
     ss.make_struct << "\n" << depth << "struct " << BigWord(key) << " {\n";
-    init_func <<"\n" << n_depth << "void FromToml(std::shared_ptr<cpptoml::base> ptr){\n";
-    for (auto &item : *ptr->as_table()) {
-      Print(item.second, item.first, ss, n_depth);
-      InitPrint(item.second, item.first, init_func, n_depth);
+    init_func << "\n" << n_depth << "void FromToml(std::shared_ptr<cpptoml::base> ptr){\n";
+    for (auto &item : type_table) {
+      MakeStructDefine(item.second, item.first, ss, n_depth);
+      MakeInitFunc(item.second, item.first, init_func, n_depth);
     }
-    init_func << n_depth <<"}\n";
-    ss.make_struct << init_func.str();
-    ss.make_struct << depth << "};\n";
-
-  } else if (ptr->is_table_array()) {
-    ss.make_struct << "\n" << depth << "struct " << BigWord(key) << " {\n";
-    init_func <<"\n" << n_depth << "void FromToml(std::shared_ptr<cpptoml::base> ptr){\n";
-    for (auto &item : **ptr->as_table_array()->begin()) {
-      Print(item.second, item.first, ss, n_depth);
-      InitPrint(item.second, item.first, init_func, n_depth);
-    }
-    init_func << n_depth <<"}\n";
+    init_func << n_depth << "}\n";
     ss.make_struct << init_func.str();
     ss.make_struct << depth << "};\n";
   }
 }
 
+} // end namespace toml_cpp
+
+using namespace toml_cpp;
 int main(int argc, char *argv[]) {
-  auto root = cpptoml::parse_file(argv[1]);
-  auto file_name = std::string(basename(argv[1]));
-  auto hpp = file_name.substr(0, file_name.find("."));
+  std::string file;
+  auto tab = 2;
+  if (argc < 3) {
+    std::cout << "toml_cpp -file file [-tab 2] [-toml_base 'third']\n";
+    return -1;
+  }
+  for (int i = 2; i < argc; i = i + 2) {
+    if (strcmp(argv[i - 1], "-file") == 0) {
+      file = argv[i];
+    } else if (strcmp(argv[i - 1], "-tab") == 0) {
+      tab = std::stoi(argv[i]);
+    } else if (strcmp(argv[i - 1], "-toml_base") == 0) {
+      toml_base_dir = argv[i];
+    } else {
+      std::cout << "not find option: " << argv[i - 1] << std::endl;
+      return -2;
+    }
+  }
+  for (auto i = 0; i < tab; i++) {
+    Tab.push_back(' ');
+  }
 
-  CppOut out;
-  PrintStruct(root, "root", out, "");
+  try {
+    auto root = cpptoml::parse_file(file);
+    auto file_name = std::string(basename(file.c_str()));
+    auto hpp = file_name.substr(0, file_name.find("."));
 
-  std::ofstream fout(hpp + "_toml.hpp", std::ios::out);
-  fout << "#pragma once\n";
-  fout << "#include \"toml_base.hpp\"\n\n";
-  fout << "namespace " <<hpp <<"_toml {\n";
-  fout << out.make_struct.str()<<"\n";
-  fout << "} // end "<< hpp <<"_toml\n";
-  fout.close();
-  std::cout << "finish toml cpp:" << file_name << std::endl;
+    CppOut out;
+    PrintStruct(root, "root", out, "");
+
+    std::ofstream fout(hpp + "_toml.hpp", std::ios::out);
+
+    if (!toml_base_dir.empty()) {
+      if(toml_base_dir.back() != '/') toml_base_dir.push_back('/');
+    }
+    fout << "#pragma once\n";
+    fout << "#include \""<< toml_base_dir << "toml_base.hpp\"\n\n";
+    fout << "namespace " << hpp << "_toml {\n";
+    fout << out.make_struct.str() << "\n";
+    fout << "} // end " << hpp << "_toml\n";
+    fout.close();
+    std::cout << "finish toml cpp:" << file_name << std::endl;
+  } catch (std::exception e) {
+    std::cout << "parse file failed :" << argv[1] << " e:"<<e.what();
+  }
+  return 0;
 }
